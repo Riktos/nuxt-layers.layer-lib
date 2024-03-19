f77620042e93ae693a3e0d300e5c97e72d15f02e
Андрей Просветкин <aprosvetkin@iproject.ru>
2024-03-19 14:59:25 +0300

    first commit

---
+++ components/CustomComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div style="background-color: aqua;" @click="onClick">CustomComponent</div>
+</template>
+
+<script setup>
+const route = useRoute()
+
+const onClick = () => {
+  console.log('click on custom component in:',route.path)
+}
+</script>
